4906289bb545ffbf47dc55c73c5a26706c292920
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2023-05-16 22:52:47 -0600

    Se arreglo el error en infoUser

--- gestor-de-hoteles-frontend/src/pages/InfoUserPage/InfoUserPage.jsx
+++ gestor-de-hoteles-frontend/src/pages/InfoUserPage/InfoUserPage.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import './InfoUserPage.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
-
+import React from 'react'
+import './InfoUserPage.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 export const InfoUserPage = () => {
   return (
